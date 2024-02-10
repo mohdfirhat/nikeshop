0d181cd338c3d0de599787aab178f4c3e0e4ae76
Mohd Firhat <mohd.firhat@hotmail.com>
2024-02-11 00:59:13 +0800

    remove types and types root from package.json and add interface to vite-env.d.ts

--- Frontend/src/vite-env.d.ts
+++ Frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_NODE_ENV: string;
+  readonly VITE_PROD_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
